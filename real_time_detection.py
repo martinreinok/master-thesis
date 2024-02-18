b17fffe50f2fd08f4db16d9189b19204c7970832
Martin Reinok <martin.reinok@gmail.com>
2024-02-18 18:28:38 +0100

    Started with plane movement algorithm. Reworked main program to threads instead of async

--- real_time_detection.py
+++ real_time_detection.py
@@ -3,6 +3,8 @@ https://github.com/MIC-DKFZ/nnUNet/blob/master/nnunetv2/inference/examples.py
 - Calibration phase
 - Tracking phase
 """
+import threading
+
 # %matplotlib inline
 from skimage import measure
 import torch
@@ -16,8 +18,13 @@ import json
 from types import SimpleNamespace
 import time
 import base64
-import accessi as Access
-from asyncio import LifoQueue
+import accessi_local as Access
+import queue as ThreadsafeQueue
+
+threadsafe_queue = ThreadsafeQueue.LifoQueue()
+opencv_input_image_queue = ThreadsafeQueue.LifoQueue()
+opencv_output_image_queue = ThreadsafeQueue.LifoQueue()
+slice_movement_requests = ThreadsafeQueue.LifoQueue()
 
 
 class CNNModel:
@@ -47,34 +54,41 @@ class CNNModel:
                                                        use_folds=folds)
         return predictor
 
-    def image_callback_cnn(self, image_data):
-        if "imageStream" in image_data:
-            start_time = time.time()
-            image_data = json.loads(json.dumps(image_data), object_hook=lambda d: SimpleNamespace(**d))
-            image_buffer = image_data[2].value.image.data
-            image_timestamp = datetime.strptime(image_data[2].value.image.acquisition.time, '%H%M%S.%f')
-            decoded_data = base64.b64decode(image_buffer)
-            try:
-                image_array = np.frombuffer(decoded_data, dtype=np.uint16)
-                image_array = np.reshape(image_array, (image_data[2].value.image.dimensions.columns,
-                                                       image_data[2].value.image.dimensions.rows))
-                # How to do this conversion properly, 16-bit to 8-bit, but the image max usually caps at ~1400
-                image = (image_array / image_array.max() * 255).astype(np.uint8)
-                image = cv2.resize(image, (512, 512)).astype(np.float32) / 255.0
-                cnn_input = image.reshape(1, 1, image.shape[0], image.shape[1])
-                props = {'spacing': (999, 1, 1)}
-                output = self.model.predict_single_npy_array(cnn_input, props, None, None, True)[0]
-                output_display = (output * 255).astype(np.uint8).reshape(512, 512)
-                image_display = (image * 255).astype(np.uint8)
-                cv2.imshow("Input Image", image_display)
-                cv2.imshow("Output Image", output_display)
-                cv2.waitKey(1)
-            except Exception as error:
-                print(f"Error in callback: {error}")
-            print("Image processing: %s seconds" % (time.time() - start_time))
-            latency = (datetime.strptime(datetime.now().strftime('%H%M%S.%f'), '%H%M%S.%f') - image_timestamp).total_seconds()
-            print(f"Image timestamp: {image_timestamp.time()} | "
-                  f"Latency: {latency} seconds")
+    def predict(self, image_data):
+        try:
+            image = cv2.resize(image_data, (512, 512)).astype(np.float32) / 255.0
+            cnn_input = image.reshape(1, 1, image.shape[0], image.shape[1])
+            props = {'spacing': (999, 1, 1)}
+            return image, self.model.predict_single_npy_array(cnn_input, props, None, None, True)[0]
+        except Exception as error:
+            print(f"Error occurred in prediction: {error}")
+        return None
+
+    def callback_thread(self):
+        while True:
+            # create a copy from the queue
+            while threadsafe_queue.empty():
+                time.sleep(0.1)
+            else:
+                image_data = threadsafe_queue.queue[-1]
+                if "imageStream" in image_data:
+                    start_time = time.time()
+                    image_data = json.loads(json.dumps(image_data), object_hook=lambda d: SimpleNamespace(**d))
+                    image_buffer = image_data[2].value.image.data
+                    image_timestamp = datetime.strptime(image_data[2].value.image.acquisition.time, '%H%M%S.%f')
+                    decoded_data = base64.b64decode(image_buffer)
+                    image_array = np.frombuffer(decoded_data, dtype=np.uint16)
+                    image_array = np.reshape(image_array, (
+                        image_data[2].value.image.dimensions.columns,
+                        image_data[2].value.image.dimensions.rows))
+                    image = (image_array / image_array.max() * 255).astype(np.uint8)
+                    input_image, output_image = self.predict(image)
+                    opencv_input_image_queue.put(input_image)
+                    opencv_output_image_queue.put(output_image)
+                    print("Image processing: %s seconds" % (time.time() - start_time))
+                    latency = (datetime.strptime(datetime.now().strftime('%H%M%S.%f'),
+                                                 '%H%M%S.%f') - image_timestamp).total_seconds()
+                    print(f"Image timestamp: {image_timestamp.time()} | Latency: {latency} seconds")
 
 
 def draw_spline(image_data):
@@ -88,58 +102,56 @@ def draw_spline(image_data):
     return spline_points
 
 
-"""
-Client IP: 192.168.182.20
-Subnet: 255.255.255.0
-Gateway: 192.168.182.0
-DNS1: 192.168.182.1
-"""
-Access.config.ip_address = "127.0.0.1"  # "10.89.184.9"
-Access.config.version = "v2"
-Model = CNNModel(path_to_model_directory="MODEL_350")
-
-
-active_check = Access.Remote.get_is_active()
-if active_check is None:
-    raise SystemExit("Server not active")
-print(f"Access-i Active: {active_check.value}")
-
-version = Access.Remote.get_version()
-print(f"Access-i Version: {version.value}")
-
-register = Access.Authorization.register(name="UTwente", start_date="20231102", warn_date="20251002",
-                                         expire_date="20251102", system_id="152379",
-                                         hash="uTwo2ohlQvMNHhfrzceCRzfRSLYDAw7zqojGjlP%2BCEmqPq1IxUoyx5hOGYbiO%2FEIyiaA4oFHFB2fwTctDbRWew%3D%3D",
-                                         informal_name="Martin Reinok Python Client")
-print(f"Access-i Register: {register.result.success}, Session: {register.sessionId}")
-if not register.result.success:
-    raise SystemExit("Access-i Registering failed")
-
-image_format = Access.Image.set_image_format("raw16bit")
-
-
-async def image_processing_thread(queue: asyncio.Queue):
+def opencv_display_thread(updates_in_second=20):
+    last_image = None
+    last_output = None
     while True:
-        image_data = await queue.get()
-        while not queue.empty():
-            await queue.get()
-        Model.image_callback_cnn(image_data)
+        if not opencv_input_image_queue.empty() and not opencv_output_image_queue.empty():
+            try:
+                input_image = opencv_input_image_queue.get()
+                output_image = opencv_output_image_queue.queue[-1]
+                output_display = (output_image * 255).astype(np.uint8).reshape(512, 512)
+                image_display = (input_image * 255).astype(np.uint8)
+                cv2.imshow("Input Image", image_display)
+                cv2.imshow("Output Image", output_display)
+                last_image = image_display
+                last_output = output_display
+                cv2.waitKey(1)
+            except:
+                if last_image is not None and last_output is not None:
+                    cv2.imshow("Input Image", last_image)
+                    cv2.imshow("Output Image", last_output)
+                    cv2.waitKey(1)
+                pass
+        time.sleep(1 / updates_in_second)
+
+
+def check_collision(model_geometry_data, prediction_image):
+    pass
+
+
+def follow_guidewire(model_geometry_data, prediction_image):
+    pass
+
+
+async def get_websocket_data(connected_event: asyncio.Event, queue: ThreadsafeQueue):
+    async with await Access.connect_websocket() as websocket:
+        connected_event.set()
+        while True:
+            message = await websocket.recv()
+            decoded_message = Access.handle_websocket_message(message)
+            queue.put(decoded_message)
 
 
 async def main():
-    lifo_queue = LifoQueue()
-
     # Run websocket
     websocket_connected_event = asyncio.Event()
-    asyncio.create_task(Access.connect_websocket(lifo_queue, websocket_connected_event))
+    asyncio.create_task(get_websocket_data(connected_event=websocket_connected_event, queue=threadsafe_queue))
     await websocket_connected_event.wait()
 
     # Connect the image service to websocket
     image_service = Access.Image.connect_to_default_web_socket()
     print(f"Access-i ImageServiceConnection: {image_service}")
-
-    # Start image processing thread
-    asyncio.create_task(image_processing_thread(lifo_queue))
 
     while True:
         await asyncio.sleep(1)
@@ -147,4 +159,25 @@ async def main():
 
 # Run the asyncio event loop
 if __name__ == "__main__":
+    """
+    Client IP: 192.168.182.20
+    Subnet: 255.255.255.0
+    Gateway: 192.168.182.0
+    DNS1: 192.168.182.1
+    """
+    Access.config.ip_address = "127.0.0.1"  # "10.89.184.9"
+    Access.config.version = "v2"
+    Model = CNNModel(path_to_model_directory="MODEL_350")
+    register = Access.Authorization.register(name="UTwente", start_date="20231102", warn_date="20251002",
+                                             expire_date="20251102", system_id="152379",
+                                             hash="uTwo2ohlQvMNHhfrzceCRzfRSLYDAw7zqojGjlP%2BCEmqPq1IxUoyx5hOGYbiO%2FEIyiaA4oFHFB2fwTctDbRWew%3D%3D",
+                                             informal_name="Martin Reinok Python Client")
+    print(f"Access-i Register: {register.result.success}, Session: {register.sessionId}")
+    if not register.result.success:
+        raise SystemExit("Access-i Registering failed")
+    image_format = Access.Image.set_image_format("raw16bit")
+    output_display_thread = threading.Thread(target=opencv_display_thread, daemon=True)
+    cnn_prediction_thread = threading.Thread(target=Model.callback_thread, daemon=True)
+    output_display_thread.start()
+    cnn_prediction_thread.start()
     asyncio.run(main())
